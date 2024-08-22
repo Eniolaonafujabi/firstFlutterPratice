e34f83e83a56f7f159a4ecd7f2fa7f7c0150ef34
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-08-22 17:38:13 +0100

    flutter app

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(
+           title: Text("My app"),
+           backgroundColor: Colors.deepPurple,
+           elevation: 0,
+           leading: Icon(Icons.menu),
+           actions: [
+            IconButton(
+              onPressed:() {},
+               icon: Icon(Icons.logout))
+           ],
+        ),
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(20)
+            ), 
+            padding: EdgeInsets.all(25), 
+            // child: Text(
+            //   "Eniola doing great",
+            //    style: TextStyle(
+            //     color: Colors.white, 
+            //     fontSize: 28,
+            //     fontWeight: FontWeight.bold,
+            //   ),
+            // ),
+            child: Icon(
+              Icons.favorite,   
+             color: Colors.white,
+             size: 64,
+             ),
+          )
+        ),
+      ),
+    );
+  }
+}
